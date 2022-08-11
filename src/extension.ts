772a2526664b2670775808fc85f8ae3b47ba7f77
Onda Reki <rekionda0524@ezweb.ne.jp>
2022-08-11 18:19:28 +0900

    statusBarItem作成

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "error-recorder" is now active!');
+const extensionCommand: string = 'error-recorder.errorRecorder';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('error-recorder.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from error-recorder!');
+export function activate(context: vscode.ExtensionContext) {
+	let disposable: vscode.Disposable = vscode.commands.registerCommand(extensionCommand, async () => {
+
+		const errorCode: string | undefined = await vscode.window.showInputBox({
+			title: 'Please enter error code.'
+		});
+
+		if (errorCode !== undefined) {
+			vscode.window.showInformationMessage(`Hello World from error-recorder!`);
+		};
 	});
 
 	context.subscriptions.push(disposable);
+
+	// StatusBarItem
+	const statusBarButton: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 0);
+	statusBarButton.command = extensionCommand;
+	statusBarButton.text = 'ErrorRecorder';
+	context.subscriptions.push(statusBarButton);
+	statusBarButton.show();
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
