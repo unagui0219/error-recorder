bfc48d98bf700ea351f6c8ff8920d13af31ad521
unagibuchi <striker0219@gmail.com>
2022-09-14 15:49:26 +0900

    showページのスタイルを修正

--- webviews/components/ViewShow.svelte
+++ webviews/components/ViewShow.svelte
@@ -2,53 +2,74 @@
 	let post = postOneData[1];
 </script>
 
-<div class="title-box">
-	<h1>{post.title}</h1>
-	<hr />
-</div>
-
-<div class="post-box">
-	<div class="post-box_content">
-		<h2 class="title">{post.title}</h2>
-		<p><span>解決策：</span>{post.solutionCode}</p>
+<div class="page-box">
+	<div class="title-box">
+		<h1>エラー内容</h1>
 	</div>
-	<div class="post-box_button_box">
-		<button class="big-btn">詳細を見る</button>
-		<button>編集</button>
-		<button class="delete-btn">削除</button>
+	<div class="top-block">
+		<div class="error-title-box">
+			<h2>{post.title}</h2>
+			<p><span>使用言語：</span>{post.lang}</p>
+		</div>
+		<div class="btn-box_cover">
+			<div class="btn-box">
+				<button>編集</button>
+				<button class="delete-btn">削除</button>
+			</div>
+		</div>
+	</div>
+	<div class="bottom-block">
+		<div class="bottom-block_section">
+			<h3>ソースコード</h3>
+			<hr />
+			<p>{post.sourceCode}</p>
+		</div>
+		<div class="bottom-block_section">
+			<h3>解決策</h3>
+			<hr />
+			<p>{post.solutionCode}</p>
+		</div>
 	</div>
 </div>
 
 <style>
+	.page-box {
+		padding: 2rem 10% 0;
+	}
+
+	.top-block {
+		margin-bottom: 2rem;
+	}
+	.error-title-box h2 {
+		font-size: 2.3rem;
+		margin-bottom: 1rem;
+	}
+	.bottom-block_section {
+		margin-bottom: 2rem;
+	}
+
+	.bottom-block_section h3 {
+		font-size: 1.8rem;
+	}
+
 	.title-box {
 		margin-top: 1rem;
 		margin-bottom: 2rem;
 		color: #c7254e;
 	}
-	.post-box {
-		border: 2px solid white !important;
-		padding: 1rem;
-		margin-bottom: 2rem;
+	.title-box h1 {
+		font-size: 3rem;
 	}
-	.post-box_content {
-		margin-bottom: 1rem;
+	.btn-box_cover {
+		display: flex;
+		justify-content: end;
 	}
-	.post-box_content h2 {
-		font-size: 2rem;
-		padding-bottom: 1rem;
+	.btn-box {
+		display: flex;
+		width: 30%;
 	}
-	.post-box_content span {
-		font-weight: bold;
-	}
-	.post-box_button_box {
-		text-align: right;
-	}
-	.post-box_button_box button {
-		width: 3rem;
-		margin-right: 1rem;
-	}
-	.big-btn {
-		width: 6rem !important;
+	.btn-box button {
+		margin-left: 1rem;
 	}
 	.delete-btn {
 		background-color: #c7254f !important;
